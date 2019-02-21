19718274fa741dcb4a380ae65af51c78bdc2ad08
andrewmcintosh <abmcintosh7@gmail.com>
2019-02-21 16:31:42 -0500

    done with post reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -10,13 +10,15 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
-    console.log('reducer')
       return {
         ...state,
         items: action.payload
       };
-      case NEW_POST:
-        
+    case NEW_POST:
+      return {
+        ...state,
+        items: action.payload
+      };
     default:
       return state;
   }
